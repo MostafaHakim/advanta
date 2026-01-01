6173cc786a199d2b8318c2c4e97c9709a884a49b
MostafaHakim <mdmostafahakim@gmail.com>
2026-01-02 01:12:13 +0600

    admin

--- lib/url.ts
+++ lib/url.ts
@@ -2,6 +2,9 @@ export function getBaseUrl() {
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
   // Fallback for local development
   return "http://localhost:3000";
 }
