46f040cfade4e150e83841f01fea5366909dcdc0
MostafaHakim <mdmostafahakim@gmail.com>
2026-03-23 02:24:08 +0600

    scioalUrl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,142 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import ClientLayoutWrapper from "@/components/layouts/ClientLayoutWrapper";
+// import { ToastContainer } from "react-toastify";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: "Digital Marketing Agency | Grow Your Business Online",
+//     template: "%s | NextMarketing",
+//   },
+//   description:
+//     "Premier digital marketing agency specializing in SEO, Social Media Marketing, PPC, and Web Development. Boost your online presence with our expert team.",
+//   keywords: [
+//     "digital marketing",
+//     "seo",
+//     "social media marketing",
+//     "web development",
+//     "ppc",
+//   ],
+//   authors: [{ name: "NextMarketing Agency" }],
+//   creator: "NextMarketing",
+//   publisher: "NextMarketing Agency",
+//   formatDetection: {
+//     email: false,
+//     address: false,
+//     telephone: false,
+//   },
+//   metadataBase: new URL("https://nextmarketing.vercel.app"),
+//   alternates: {
+//     canonical: "/",
+//   },
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     url: "https://nextmarketing.vercel.app",
+//     title: "Digital Marketing Agency | Grow Your Business Online",
+//     description:
+//       "Premier digital marketing agency specializing in SEO, Social Media Marketing, PPC, and Web Development.",
+//     siteName: "NextMarketing",
+//     images: [
+//       {
+//         url: "/og-image.png",
+//         width: 1200,
+//         height: 630,
+//         alt: "NextMarketing Agency",
+//       },
+//     ],
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Digital Marketing Agency | Grow Your Business Online",
+//     description:
+//       "Premier digital marketing agency specializing in SEO, Social Media Marketing, PPC, and Web Development.",
+//     images: ["/twitter-image.png"],
+//     creator: "@nextmarketing",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html
+//       lang="en"
+//       className="scroll-smooth overflow-x-hidden"
+//       suppressHydrationWarning={true}
+//     >
+//       <head>
+//         {/* Google Tag Manager */}
+//         <script
+//           dangerouslySetInnerHTML={{
+//             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//       })(window,document,'script','dataLayer','GTM-NWP7GGLP');`,
+//           }}
+//         />
+
+// <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '871201332274380');
+// fbq('track', 'PageView');
+// </script>
+
+//       </head>
+//       <body
+//         className={`${inter.className} bg-white text-gray-900 overflow-x-hidden w-screen`}
+//       >
+//         <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+//         <ToastContainer />
+
+//         {/* Google Tag Manager (noscript) */}
+//         <noscript>
+//           <iframe
+//             src="https://www.googletagmanager.com/ns.html?id=GTM-NWP7GGLP"
+//             height="0"
+//             width="0"
+//             style={{ display: "none", visibility: "hidden" }}
+//           />
+//         </noscript>
+//         <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=871201332274380&ev=PageView&noscript=1"
+// /></noscript>
+//         {/* End Google Tag Manager (noscript) */}
+//       </body>
+//     </html>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ClientLayoutWrapper from "@/components/layouts/ClientLayoutWrapper";
 import { ToastContainer } from "react-toastify";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,15 +156,13 @@ export const metadata: Metadata = {
   authors: [{ name: "NextMarketing Agency" }],
   creator: "NextMarketing",
   publisher: "NextMarketing Agency",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
+
   metadataBase: new URL("https://nextmarketing.vercel.app"),
+
   alternates: {
     canonical: "/",
   },
+
   openGraph: {
     type: "website",
     locale: "en_US",
@@ -48,6 +180,7 @@ export const metadata: Metadata = {
       },
     ],
   },
+
   twitter: {
     card: "summary_large_image",
     title: "Digital Marketing Agency | Grow Your Business Online",
@@ -56,16 +189,10 @@ export const metadata: Metadata = {
     images: ["/twitter-image.png"],
     creator: "@nextmarketing",
   },
+
   robots: {
     index: true,
     follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
   },
 };
 
@@ -75,28 +202,38 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className="scroll-smooth overflow-x-hidden"
-      suppressHydrationWarning={true}
-    >
-      <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NWP7GGLP');`,
-          }}
-        />
-      </head>
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} bg-white text-gray-900 overflow-x-hidden w-screen`}
+        className={`${inter.className} bg-white text-gray-900 overflow-x-hidden`}
       >
-        <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
-        <ToastContainer />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NWP7GGLP');
+          `}
+        </Script>
+
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '871201332274380');
+            fbq('track', 'PageView');
+          `}
+        </Script>
 
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -107,7 +244,19 @@ export default function RootLayout({
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
+        {/* Facebook Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=871201332274380&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+        <ToastContainer />
       </body>
     </html>
   );
